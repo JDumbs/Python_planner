184bd774132cca86c1f27ee71a60d686449ed6de
James-W-Pettit <108548172+JDumbs@users.noreply.github.com>
2023-03-30 08:48:22 -0500

    added view function

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ def note():
     f.write(note_contents)
     f.close()
 
+def view():
+    print("View a [note] or a [timer], or [alarm]")
+    choice = input("> ")
+    if choice == "note":
+        print("What is the name of your note?")
+        note_name = input("> ")
+        f = open(note_name+".txt", "r")
+        print(f.read())
 
 
 print("Welcome to PyPlanner were we help you get on track")
 print("Add a new [note] or start a new [timer],[alarm],[settings],or [quit]")
+print("You can also [view]")
 while True:
+    print("")
     choice = input("> ")
     if choice == "note":
         note()
+    if choice == "view":
+        view()
 
